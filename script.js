1975dbc9dffa61def55087ac0dd8c88347eb6571
wedho <wellingtond99@gmail.com>
2024-04-04 05:20:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+// Smooth scroll to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggler = document.getElementById("toggler");
+    const navbar = document.getElementById("navbar");
+
+    // Initialize navbar state
+    if (window.innerWidth <= 768) {
+        navbar.style.display = "none";
+    }
+
+    // Toggle navbar visibility and highlight button on small screens
+    const toggleNavbar = () => {
+        if (navbar.style.display === "none") {
+            navbar.style.display = "block";
+            toggler.style.border = "3px solid crimson"; // Highlighting the button
+        } else {
+            navbar.style.display = "none";
+            toggler.style.border = "none";
+        }
+    }
+
+    // Add click event listener to the toggler button
+    toggler.addEventListener("click", toggleNavbar);
+
+    // Close navbar when a menu item is clicked
+    const menuItems = document.querySelectorAll('.menu-items a');
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navbar.style.display = "none";
+            toggler.style.border = "none";
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const modalButtons = document.querySelectorAll('.readmore-btn');
+    const modals = document.querySelectorAll('.modal');
+
+    modalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modalId = button.getAttribute('data-modal');
+            const modal = document.getElementById(modalId);
+            modal.style.display = 'block';
+        });
+    });
+
+    modals.forEach(modal => {
+        const closeButton = modal.querySelector('.close');
+        closeButton.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+
+        window.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+    });
+});
+
+
